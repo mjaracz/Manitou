1392d821b51eb292b4f9e4c4c39ae8fc7463fec7
mjaracz <michal19996@gmail.com>
2019-06-07 14:24:33 +0200

    preper app to deploy on Heroku

--- back-end/index.js
+++ back-end/index.js
@@ -1,26 +1,23 @@
 const parser = require('body-parser');
 const routes = require('./stories/stories.routes.js');
 const userRoutes = require('./users/user.routes');
-const cors = require('cors');
 
 const express = require('express');
 
 const app = express();
-const http = require('http').Server(app);
-
 const port = process.env.PORT || 8080;
 
-app.use(cors());
 app.use(parser.json());
-app.use(parser.urlencoded({extended: true}));
+app.use(parser.urlencoded({ extended: true }));
 app.use('/api', routes);
-app.use('/api/user', userRoutes);
+app.use('/user', userRoutes);
 app.use((req, res) => {
-  res.status(404)
+  res.send(JSON.stringify({"message": "route not handler"}));
+  res.status(404);
 });
 
-http.listen(port, () => {
-  console.log('server started on port ' + port)
+app.listen(port, () => {
+  console.log("server started on port " + port)
 });
 
 module.exports = app;
